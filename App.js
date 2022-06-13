a8a8f2a72a85e4220c185e09298098dd927c2d4f
nandia17 <107383998+nandia17@users.noreply.github.com>
2022-06-13 13:03:46 +0800

    Add files via upload
    hkjhkj

---
+++ App.js
@@ -0,0 +1,167 @@
+
+import './App.css';
+import Img from './components/img';
+import Header from './components/header';
+import Para from './components/para';
+import Une from './components/une';
+
+function App() {
+  return (
+    <div className="App">
+     <div className='nav'>
+         <h1>Our Menu</h1>
+         <div className='zuraas'></div>
+         <div className='nav-menu'>
+           
+              <a href='#'>All</a>
+            <a href='#'>Breakfast</a>
+            <a href='#'>Lunch</a>
+              <a href='#'>Shakes</a>
+          
+         </div>
+     </div>
+     <div className='menu-box'>
+        <div className='box-1'>
+             <div className='img-div'>
+                   <Img src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+              </div>
+              <div className='text'>
+                      <div className='text-a'>
+                           <Header title="Buttermilk Pancakes"/> 
+                           <Une tit="$15"/>
+                      </div>
+              
+                      
+                      
+                      <Para titlee="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque, libero sit amet congue semper, elit velit vulputate erat, at vulputate diam lorem ut ex. Praesent neque tellus, sodales id congue sed, porta eu massa.
+                      Vivamus faucibus mauris ac lacus tempus, a mollis dolor egestas. Phasellus dignissim lorem magna, quis mattis ante venenatis vitae. Curabitur scelerisque c"/>
+               </div>
+         
+        </div>
+
+        <div className='box-1'>
+             <div className='img-div'>
+                   <Img src="https://images.pexels.com/photos/2280573/pexels-photo-2280573.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+              </div>
+              <div className='text'>
+                      <div className='text-a'>
+                           <Header title="Diner Double"/> 
+                           <Une tit="$15"/>
+                      </div>
+              
+                      
+                      
+                      <Para titlee="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque, libero sit amet congue semper, elit velit vulputate erat, at vulputVivamus faucibus mauris ac lacus tempus, a mollis dolor egestas. Phasellus dignissim lorem magna, quis mattis ante venenatis vitae. Curabitur scelerisque cate diam lorem ut ex. Praesent neque tellus, sodales id congue sed, porta eu massa."/>
+               </div>
+         
+        </div>
+        <div className='box-1'>
+             <div className='img-div'>
+                   <Img src="https://images.pexels.com/photos/2638026/pexels-photo-2638026.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+              </div>
+              <div className='text'>
+                      <div className='text-a'>
+                           <Header title="Godzilla Milkshake"/> 
+                           <Une tit="$15"/>
+                      </div>
+              
+                      
+                      
+                      <Para titlee="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque, libero sit amet congue semper, elit velit vulputate erat, at vulputVivamus faucibus mauris ac lacus tempus, a mollis dolor egestas. Phasellus dignissim lorem magna, quis mattis ante venenatis vitae. Curabitur scelerisque cate diam lorem ut ex. Praesent neque tellus, sodales id congue sed, porta eu massa."/>
+               </div>
+         
+        </div>
+        <div className='box-1'>
+             <div className='img-div'>
+                   <Img src="https://images.pexels.com/photos/4346305/pexels-photo-4346305.jpeg?auto=compress&cs=tinysrgb&w=1600"/>
+              </div>
+              <div className='text'>
+                      <div className='text-a'>
+                           <Header title="Country Delight"/> 
+                           <Une tit="$15"/>
+                      </div>
+              
+                      
+                      
+                      <Para titlee="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque, libero sit amet congue semper, elit velit vulputate erat, at vulputVivamus faucibus mauris ac lacus tempus, a mollis dolor egestas. Phasellus dignissim lorem magna, quis mattis ante venenatis vitae. Curabitur scelerisque cate diam lorem ut ex. Praesent neque tellus, sodales id congue sed, porta eu massa."/>
+               </div>
+         
+        </div>
+        <div className='box-1'>
+             <div className='img-div'>
+                   <Img src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+              </div>
+              <div className='text'>
+                      <div className='text-a'>
+                           <Header title="Buttermilk Pancakes"/> 
+                           <Une tit="$15"/>
+                      </div>
+              
+                      
+                      
+                      <Para titlee="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque, libero sit amet congue semper, elit velit vulputate erat, at vulputate diam lorem ut ex. Praesent neque tellus, sodales id congue sed, porta eu massa.
+                      Vivamus faucibus mauris ac lacus tempus, a mollis dolor egestas. Phasellus dignissim lorem magna, quis mattis ante venenatis vitae. Curabitur scelerisque c"/>
+               </div>
+         
+        </div>
+        <div className='box-1'>
+             <div className='img-div'>
+                   <Img src="https://images.pexels.com/photos/2280573/pexels-photo-2280573.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+              </div>
+              <div className='text'>
+                      <div className='text-a'>
+                           <Header title="Diner Double"/> 
+                           <Une tit="$15"/>
+                      </div>
+              
+                      
+                      
+                      <Para titlee="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque, libero sit amet congue semper, elit velit vulputate erat, at vulputVivamus faucibus mauris ac lacus tempus, a mollis dolor egestas. Phasellus dignissim lorem magna, quis mattis ante venenatis vitae. Curabitur scelerisque cate diam lorem ut ex. Praesent neque tellus, sodales id congue sed, porta eu massa."/>
+               </div>
+         
+        </div>
+        <div className='box-1'>
+             <div className='img-div'>
+                   <Img src="https://images.pexels.com/photos/2638026/pexels-photo-2638026.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+              </div>
+              <div className='text'>
+                      <div className='text-a'>
+                           <Header title="Godzilla Milkshake"/> 
+                           <Une tit="$15"/>
+                      </div>
+              
+                      
+                      
+                      <Para titlee="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque, libero sit amet congue semper, elit velit vulputate erat, at vulputVivamus faucibus mauris ac lacus tempus, a mollis dolor egestas. Phasellus dignissim lorem magna, quis mattis ante venenatis vitae. Curabitur scelerisque cate diam lorem ut ex. Praesent neque tellus, sodales id congue sed, porta eu massa."/>
+               </div>
+         
+        </div>
+
+        <div className='box-1'>
+             <div className='img-div'>
+                   <Img src="https://images.pexels.com/photos/4346305/pexels-photo-4346305.jpeg?auto=compress&cs=tinysrgb&w=1600"/>
+              </div>
+              <div className='text'>
+                      <div className='text-a'>
+                           <Header title="Country Delight"/> 
+                           <Une tit="$15"/>
+                      </div>
+              
+                      
+                      
+                      <Para titlee="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque, libero sit amet congue semper, elit velit vulputate erat, at vulputVivamus faucibus mauris ac lacus tempus, a mollis dolor egestas. Phasellus dignissim lorem magna, quis mattis ante venenatis vitae. Curabitur scelerisque cate diam lorem ut ex. Praesent neque tellus, sodales id congue sed, porta eu massa."/>
+               </div>
+         
+        </div>
+        
+     </div>
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// massive-t hadgalaad for dawtaltaar  nemegdeheer  
---
+++ index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+
+function Une(props) {
+    return (  
+        <div className="une-garchig">
+        <p>{props.tit}</p>
+      </div>
+    );
+}
+
+export default Une;
